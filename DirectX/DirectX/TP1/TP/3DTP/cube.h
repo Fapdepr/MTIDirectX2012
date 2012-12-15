e6c8c2f9044cd02943ea24762c01b76168bc0620
fapdep_q <fapdeprquentin@gmail.com>
2012-12-15 23:09:50 +0100

    added new proj

--- DirectX/DirectX/TP1/TP/3DTP/cube.h
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "3DTP.h"
-#include <d3d9.h>
-#include <d3dx9.h>
-
-int fillVertexBuffer (vertex* pData, int index, int face, int sizeSquare);
-int fillIndexBuffer(int* pDataIndex, int offset, int k, int s1, int s2, int s3, int s4, int sizeSquare);
